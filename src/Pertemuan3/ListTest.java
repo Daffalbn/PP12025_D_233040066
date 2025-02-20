33861bfe22cd1de655c1ee681d15d82da14cbe11
Daffalbn <daffaaibani56@gmail.com>
2025-02-20 23:50:27 +0700

    mengerjakan modul 3

---
+++ src/Pertemuan3/ListTest.java
@@ -0,0 +1,23 @@
+package Pertemuan3;
+
+public class ListTest {
+
+
+	    public static void main(String[] args) {
+	        StrukturList list = new StrukturList();
+	       //list.addTail(1);
+	       //list.addTail(4);
+	       //list.addTail(5);
+	       //list.addTail(7);
+	        
+
+	        System.out.println("Elemen: ");
+	        list.addHead(7);
+	        list.addHead(5);
+	        list.addHead(4);
+	        list.addHead(1);
+	        list.displayElement();
+	    }
+	
+
+}
